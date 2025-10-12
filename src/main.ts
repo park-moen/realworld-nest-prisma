f8e93549c3751ef35e104c69521cdfa9ce340cb8
park-moen <pmj6516@naver.com>
2025-10-12 16:57:34 +0900

    feat(core): add global ClassSerializerInterceptor and Logger in main.ts
    - enable global serialization via ClassSerializerInterceptor
    - integrate NestJS Logger for application startup logs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Todo(Refactor): Custom Logger로 만들기 & 설정 추가하기
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger(),
+  });
   app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(3000);
 }
 bootstrap();
