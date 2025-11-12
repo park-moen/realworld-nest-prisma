f7bd102dbfc3d127688f1cc914a499f92d03a38b
park-moen <pmj6516@naver.com>
2025-11-12 22:24:38 +0900

    feat: Trim 관련 custom Transform 구현 & Dto에 적용
    - @Trim 커스텀 데코리이터를 만들어서 빈 문자열, 공백 빈 문자열, 공백을 제거
    - CreateArticleDto에 적용
    - UpdateArticleDto에 적용

--- src/article/dto/request/create-article.dto.ts
+++ src/article/dto/request/create-article.dto.ts
@@ -1,14 +1,19 @@
+import { Trim } from '@app/common/transforms/trim.transform';
 import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
+// ! 빈 문자열이 들어오지 못하게 검증
 export class CreateArticleDto {
+  @Trim()
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @Trim()
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @Trim()
   @IsString()
   @IsNotEmpty()
   body: string;
--- src/article/dto/request/update-article.dto.ts
+++ src/article/dto/request/update-article.dto.ts
@@ -1,21 +1,26 @@
+import { Trim } from '@app/common/transforms/trim.transform';
 import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateArticleDto {
+  @Trim()
   @IsString()
   @IsNotEmpty()
   @IsOptional()
   title?: string;
 
+  @Trim()
   @IsString()
   @IsNotEmpty()
   @IsOptional()
   description?: string;
 
+  @Trim()
   @IsOptional()
   @IsString()
   @IsNotEmpty({ message: 'Body must not be empty' })
   body?: string;
 
+  @Trim()
   @IsArray()
   @IsString({ each: true })
   @IsOptional()
