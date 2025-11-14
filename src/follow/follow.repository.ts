9b4639cb15bcb4eaefcbf7431cab2fd96d77db4d
park-moen <pmj6516@naver.com>
2025-11-14 18:50:29 +0900

    feat: User follow 기능 구현(FollowService.followUser, FollowRepository.create)

--- src/follow/follow.repository.ts
+++ src/follow/follow.repository.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@app/prisma/prisma.service';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class FollowRepository {}
+export class FollowRepository {
+  private readonly logger = new Logger(FollowRepository.name);
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(followerId: string, followingId: string): Promise<void> {
+    await this.prisma.follow.create({
+      data: {
+        followerId,
+        followingId,
+      },
+    });
+  }
+
+  async exists(followerId: string, followingId: string): Promise<boolean> {
+    const follow = await this.prisma.follow.findFirst({
+      where: {
+        followerId,
+        followingId,
+      },
+    });
+
+    return !!follow;
+  }
+}
--- src/follow/follow.service.ts
+++ src/follow/follow.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { FollowRepository } from './follow.repository';
+import {
+  AlreadyFollowingError,
+  SelfFollowNotAllowedError,
+} from '@app/common/errors/follow-domain.error';
 
 @Injectable()
-export class FollowService {}
+export class FollowService {
+  private readonly logger = new Logger(FollowService.name);
+  constructor(private readonly followRepository: FollowRepository) {}
+
+  async isFollowing(followerId: string, followingId: string): Promise<boolean> {
+    const follow = await this.followRepository.exists(followerId, followingId);
+
+    return !!follow;
+  }
+
+  async followUser(followerId: string, followingId: string): Promise<void> {
+    if (followerId === followingId) {
+      throw new SelfFollowNotAllowedError(followerId, followingId);
+    }
+
+    try {
+      await this.followRepository.create(followerId, followingId);
+    } catch (error) {
+      // ! Prisma에 의존하는 Error를 사용하고 있음
+      if (error.code === 'P2002') {
+        throw new AlreadyFollowingError(followerId, followingId);
+      }
+
+      throw error;
+    }
+  }
+}
