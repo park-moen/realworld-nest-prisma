f9138a4fa8e21fd8fddfe22d77e45b00731be601
park-moen <pmj6516@naver.com>
2025-11-14 22:13:23 +0900

    fix: OptionalAccessTokenGuard에서 부모 클래스 생성자(super)로 authService DI 전달

--- src/common/guards/optional-access-token.guard.ts
+++ src/common/guards/optional-access-token.guard.ts
@@ -1,8 +1,12 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AccessTokenGuard } from './access-token.guard';
+import { AuthService } from '@app/auth/auth.service';
 
 @Injectable()
 export class OptionalAccessTokenGuard extends AccessTokenGuard {
+  constructor(authService: AuthService) {
+    super(authService);
+  }
   canActivate(context: ExecutionContext): boolean {
     try {
       return super.canActivate(context);
