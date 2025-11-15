d8177b32eef6b3e8f96a60580f803dc765f45ef3
park-moen <pmj6516@naver.com>
2025-11-15 17:28:38 +0900

    fix: CreateCommentDto에서 불필요한 @MinLength(1) 데코레이터 제거
    - @IsNotEmpty()와 @MInLength(1)은 동일한 검증을 진행하는 데코레이터라는 점을 인지

--- src/comment/dto/request/create-comment.dto.ts
+++ src/comment/dto/request/create-comment.dto.ts
@@ -1,11 +1,10 @@
 import { Trim } from '@app/common/transforms/trim.transform';
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateCommentDto {
   @Trim()
   @IsString()
   @IsNotEmpty()
-  @MinLength(1)
   @MaxLength(1000)
   body: string;
 }
